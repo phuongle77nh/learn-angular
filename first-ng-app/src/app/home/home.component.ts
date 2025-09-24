8ed480b5c3db46142c2c208877bc26a175abff24
phuongle77nh <phuongle77nh@gmail.com>
2025-09-24 18:20:06 +0700

    Refactor app structure and enhance components
    - Updated app.component.ts to use router outlet for navigation.
    - Defined routes in app.routes.ts for home and todos components.
    - Enhanced counter.component.html and counter.component.ts with increment, reset, and decrement functionalities.
    - Improved greeting.component.html and greeting.component.ts to accept dynamic messages.
    - Modified header.component.html and header.component.ts to include navigation links.
    - Updated home.component.html and home.component.ts to integrate greeting and counter components.
    - Created todo-item component with basic structure and tests.
    - Added todos component with initial setup and tests.

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`Key Up event triggered, key ${event.key}`);
+  };
 
+  homeMessage = signal('Welcome to the Home Page!');
 }
