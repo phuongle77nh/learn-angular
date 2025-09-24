d665dfeefee901a8a2d09eb36ef0ee70035f7899
phuongle77nh <phuongle77nh@gmail.com>
2025-09-24 10:57:18 +0700

    first ng app

---
+++ first-ng-app/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+  `,
+  styles: [
+    `
+        main {
+          padding: 16px;
+        }
+    `
+  ],
+})
+export class AppComponent {
+  protected readonly title = signal('first-ng-app');
+}
